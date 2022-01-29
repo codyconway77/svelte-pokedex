61b833517d8e92e355bea9c83679c86da77b85e2
Codyconway77 <codyconway77@gmail.com>
2022-01-28 23:51:28 -0800

    fix: larger font size on pokemon stats and centered sprite

--- src/routes/[slug].svelte
+++ src/routes/[slug].svelte
@@ -26,14 +26,14 @@
         <div class="grid grid-cols-2 gap-2 py-4 content-center justify-center h-max bg-slate-400 rounded">
             <div class="flex flex-col pl-8 content-between gap-6">
                 <p class="font-bold text-3xl capitalize">Name: {currentPokeman.name}</p>
-                <p class="font-bold">ID: {currentPokeman.id}</p>
+                <p class="font-bold mt-[-15px]">ID: {currentPokeman.id}</p>
                 {#each currentPokeman.types as type}
-                    <p class="mx-1 capitalize">Type: {type.type.name}</p>    
+                    <p class="mx-1 capitalize text-xl">Type: {type.type.name}</p>    
                 {/each}
-                <p>Height: {currentPokeman.height}</p>
-                <p>Weight: {currentPokeman.weight}</p>
+                <p class="text-xl">Height: {currentPokeman.height}</p>
+                <p class="text-xl">Weight: {currentPokeman.weight}</p>
             </div>
-            <img class="justify-self-center w-4/6" src={currentPokeman.image} alt={currentPokeman.name}>
+            <img class="justify-self-center self-center w-4/6" src={currentPokeman.image} alt={currentPokeman.name}>
         </div>
     {/await}
 </div>
